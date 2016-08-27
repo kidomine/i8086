fbb574dc3e0294b128244fcc01c1a769faf4ed99
Ian Diaz <yan@artemis.local>
2016-08-27 15:14:20 +0800

    Added placeholder for unit tests. Will be update soon.

---
+++ tests/i86_test_main.c
@@ -0,0 +1,15 @@
+/*
+ * i8086/i86_test_main.c
+ *
+ *   Created on: Aug 27, 2016 3:11:43 PM
+ *       Author: yan@artemis
+ *
+ *  Description:
+ *    Reference:
+ */
+
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
